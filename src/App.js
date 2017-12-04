6e071f9d413be41fee5958a69a11e27b9d388816
David Napier <dnapier@umd.edu>
2017-12-04 10:22:28 -0500

    Added comments, removed unused variables.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  This is the entry point for the VCL application. *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Removed 'Link' to avoid warning. 
 import './App.css'; //import css files here
 
 //import logo from './logo.svg' here;
 
 //Components (import js files of all the components)
 import Header from './components/headerComponent/header'
-import Footer from './components/footerComponent/footer'
+//import Footer from './components/footerComponent/footer'
 import HomePage from './components/pages/homePage'
 //import HelpPage from './components/pages/helpPage'
 import AdminPage from './components/pages/adminPage'
