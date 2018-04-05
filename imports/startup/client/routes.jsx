8499a4db02b4b1eac143df1c5f7054a29bf71eae
David Napier <dnapier@hornbake-350.umd.edu>
2018-04-05 17:12:46 -0400

    Nested routes to be rendered within components using react-router v4

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -6,12 +6,9 @@ import createHistory from 'history/createBrowserHistory'
 import AppContainer from '../../ui/containers/AppContainer.jsx';
 import AuthPageSignIn from '../../ui/pages/AuthPageSignIn.jsx';
 //			<Route path="/signin" component={AuthPageSignIn}/>
-const createBrowserHistory = createHistory();
 
 export const renderRoutes = () => (
-	<Router history={createBrowserHistory}>
-		<AppContainer>
-			<Route path="/sigin" component={AuthPageSignIn} />
-		</AppContainer>
+	<Router>
+		<AppContainer />
 	</Router>
 );
