a1de38e4916b0db0fbfd9e7ff4323c3a060891db
David Napier <dnapier@umd.edu>
2018-04-13 21:19:06 -0400

    Merge resolutions were not saved.

--- client/main.js
+++ client/main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-
-import App from '../imports/ui/App.js';
-
-Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('app'));
-});
-=======
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes.jsx';
@@ -16,4 +5,3 @@ import { renderRoutes } from '../imports/startup/client/routes.jsx';
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
 });
->>>>>>> remotes/origin/user_accounts
