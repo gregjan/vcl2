fc35f9402b1d5897d6c1808761505534c2c38dce
David Napier <dnapier@umd.edu>
2018-04-13 21:35:13 -0400

    Updated to working release, package-lock.json should be in gitignore.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
-=======
 import '/imports/startup/server';
->>>>>>> remotes/origin/user_accounts
