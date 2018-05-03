f7fd6255fee06d89492a6e8dfe32594c76e0cf3d
abhishek7993 <abhishek.bv93@gmail.com>
2018-05-03 14:09:51 -0400

    Admin form to create a machine.

--- imports/ui/components/AddEvent.jsx
+++ imports/ui/components/AddEvent.jsx
@@ -4,37 +4,28 @@ import {FormControl} from 'react-bootstrap';
 class AddMachine extends Component {
   constructor(props) {
     super(props);
+    this.state = {OS:"", Size:"", Ports:""};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      ClassCode: "",
-      OperatingSystem: "",
-      systemSize: "",
-      ports:"",
-    }
   }
 
 handleChange(event){
     const field = event.target.name;
-    alert(field);
-    // we use square braces around the key `field` because its a variable (we are setting state with a dynamic key name)
     this.setState({
       [field]: event.target.value
     })
+    //this.setState({value: event.target.value});
+
   }
 
   handleSubmit(event){
-    event.preventDefault();
-    window.location.reload();
-    // TODO: Create backend Meteor methods to save created events
-    alert("Will be Saved in a little bit :)")
-    const { ClassCode, OperatingSystem, systemSize, ports } = this.state;
 
-    // add method `insert` to db
-    alert(ClassCode);
-     //this.setState({redirect: true }));
-    //return <Redirect to='http://localhost:3000/'  />
-  }
+    //alert('Class Code was submitted: ' + this.state.value);
+    event.preventDefault();
+    const {OS, Size, Ports} = this.state;
+    console.log("class code is: ", ClassCode, "\nOS type is: ", OS, "\nMachine Size is: ", Size, "\nPorts are: ", Ports);
+    //alert('Operating System type was submitted: ' + this.state.value);
+  };
 
   render() {
     return (
@@ -44,51 +35,29 @@ handleChange(event){
         </div>
         <hr />
 
-        <div className="jumbotron" style={{ margin: "0 500px" }}>
+        <div className="jumbotron jumbotron-fluid" style={{ margin: "0 500px" }}>
           <form onSubmit={this.handleSubmit}>
-
-            <div className="form-group">
-              <label>Class Code:</label>
-              <FormControl
-               type="text"
-               placeholder="Enter Class Code"
-               name="ClassCode"
-               value={this.state.name}
-               onChange={this.handleChange}
-              />
+            <div><label>Operating System:</label></div>
+            <div>
+              <select value={this.state.value} name="OS" value={this.state.name} onChange={this.handleChange}>
+                <option value="Select">Select</option>
+                <option value="Windows">Windows</option>
+                <option value="Ubuntu">Ubuntu</option>
+              </select>
             </div>
-
-            <div className="form-group">
-              <label>Operating System:</label>
-              <FormControl componentClass="select" placeholder="select" onChange={this.handleChange} name="OperatingSystem" value={this.state.value} >
-              <option value="Windows">Windows</option>
-              <option value="Ubuntu">Ubuntu</option>
-              </FormControl>
+            <div><label>System Size:</label></div>
+            <div>
+              <select value={this.state.value} name="Size" value={this.state.name} onChange={this.handleChange}>
+                <option value="Select">Select</option>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+              </select>
             </div>
-
-            <div className="form-group">
-              <label>System Size:</label>
-              <FormControl componentClass="select" placeholder="choose">
-              <option value="Small">Small</option>
-              <option value="Medium">Medium</option>
-              <option value="Large">Large</option>
-              name="systemSize"
-              value={this.state.value}
-              onChange={this.handleChange}
-              </FormControl>
-            </div>
-
-            <div className="form-group">
-              <label>Incoming Port:</label>
-              <FormControl
-              type="text"
-              placeholder="Enter Ports"
-              name="ports"
-              value={this.state.name}
-              onChange={this.handleChange}
-             />
-            </div>
-
+            <div><label>Software List (Separated by comma):</label></div>
+              <div><input type="text" placeholder="Enter ports" name = "Ports" value={this.state.name} onChange={this.handleChange} /></div>
+            <div><label>Incoming Ports (e.g. 22,80,443):</label></div>
+              <div><input type="text" placeholder="Enter ports" name = "Ports" value={this.state.name} onChange={this.handleChange} /></div>
             <button type="submit" className="btn btn-primary">Add Machine</button>
           </form>
         </div>
